afc9ab057c08500ec05d2ebddd29202fdd446d4b
Sadi-Ashlaq <71584001+Sadi-Ashlaq@users.noreply.github.com>
2021-02-02 23:14:56 +0200

    youtubeAPI

---
+++ src/youtube.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+import YouTube from 'react-youtube';
+
+import Style from './youtube.css';
+
+//https://youtu.be/pRTWFqhEDeo
+
+//npm install react-youtube 
+
+export default class Youtube extends Component {
+
+	_onReady(event) {
+
+    // access to player in all event handlers via event.target
+
+    event.target.pauseVideo();
+
+  }
+
+render() {
+
+    const opts = {
+
+      height: '390',
+
+      width: '75%',
+    
+      playerVars: {
+
+        // https://developers.google.com/youtube/player_parameters
+
+      autoplay: 1,
+
+      },
+
+    };
+     
+    return (
+
+     <div className = "player">
+
+    <YouTube videoId="pRTWFqhEDeo" opts={opts} onReady={this._onReady} />
+
+     </div>
+
+    );
+
+  }
+  
+}
